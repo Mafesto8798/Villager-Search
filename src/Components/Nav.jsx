3a1c50f88b1a0dc5795d47c4b30a23d7cb458155
mafesto87 <kenny.chavarriaga@gmail.com>
2020-11-28 18:10:49 -0500

    added clouds responsiveness

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,6 +4,14 @@ import {Link} from 'react-router-dom'
 export default function Nav() {
     return (
         <div className="nav-container">
+            <div className="clouds-container">
+            <img className="cloud1" src="Cloud.png" alt="cloud"/>
+            <img className="little1" src="Cloud.png" alt="cloud"/>
+            <img className="cloud2" src="Cloud.png" alt="cloud"/>
+            <img className="little2" src="Cloud.png" alt="cloud"/>
+            <img className="cloud3" src="Cloud.png" alt="cloud"/>
+            <img className="little3" src="Cloud.png" alt="cloud"/>
+            </div>
             <nav>
                 <Link className="nav-links" to="/home">
                 <h1>Home</h1>
